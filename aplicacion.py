22d609aee5958544209f297834b48f6aa186588d
gerardofuentes93@yahoo.com.mx <gerardofuentes93@yahoo.com.mx>
2025-06-16 23:18:32 -0600

    commit 4

--- aplicacion.py
+++ aplicacion.py
@@ -1,16 +1,16 @@
 import pandas as pd
 import streamlit as st
 import plotly_express as px
-car_data = pd.read_csv('vehicles_us.csv')
+car_data = pd.read_csv(
+    r'C:\Users\gerar\Documents\Tripleten proyectos\Proyecto-unidad-7\notebook\vehicles_us.csv')
 show_hist = st.checkbox('Construir histograma')
-
-if hist_button:
+scatter_button = st.checkbox('construir gráfico de dispersión')
+if show_hist:
     st.write(
         'Creación de un histograma para el conjunto de datos de anuncios de ventas totales de coches por modelos')
     fig = px.histogram(car_data, x="model", y="price")
     st.plotly_chart(fig, use_container_width=True)
 
-show_scatter = st.checkbox('Mostrar gráfico de dispersión')
 
 if scatter_button:
     st.write('Creación de un gráfico de dispersión: modelo vs precio')
